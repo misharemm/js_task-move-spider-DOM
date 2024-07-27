922629a1c32f9bcbf5fce22143a6d72b2d38839d
Misha Remeslo <misha.remeslol@gmail.com>
2024-07-27 13:58:46 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,20 @@
+"use strict";
+const wall = document.querySelector("div");
+const spider = document.querySelector(".spider");
+document.addEventListener("click", (e)=>{
+    const clickY = e.clientY;
+    const clickX = e.clientX;
+    const wallRect = wall.getBoundingClientRect();
+    const wallBorderWidth = parseFloat(getComputedStyle(wall).borderWidth);
+    let spiderX = clickX - spider.offsetWidth / 2 - wallBorderWidth;
+    let spiderY = clickY - spider.offsetHeight / 2 - wallBorderWidth;
+    if (clickX < wallRect.left || clickX > wallRect.right || clickY > wallRect.bottom || clickY < wallRect.top) return;
+    if (spiderX < wallRect.left) spiderX = wallRect.left;
+    if (spiderY < wallRect.top) spiderY = wallRect.top;
+    if (spiderX + spider.offsetWidth > wallRect.right - 2 * wallBorderWidth) spiderX = wallRect.right - spider.offsetHeight - 2 * wallBorderWidth;
+    if (spiderY + spider.offsetHeight > wallRect.bottom - 2 * wallBorderWidth) spiderY = wallRect.bottom - spider.offsetWidth - 2 * wallBorderWidth;
+    spider.style.top = `${spiderY - wallRect.top}px`;
+    spider.style.left = `${spiderX - wallRect.left}px`;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
